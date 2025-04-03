d627e04e1d98704b6392139b0320be430e97c3de
keapython <esenfirdavs1980@gmail.com>
2025-04-04 01:51:52 +0500

    added app

---
+++ app/database.py
@@ -0,0 +1,37 @@
+
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine
+
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+DB_NAME=os.getenv("DB_NAME")
+DB_HOST=os.getenv("DB_HOST")
+DB_PORT=os.getenv("DB_PORT")
+DB_USER=os.getenv("DB_USER")
+DB_PASSWORD=os.getenv("DB_PASSWORD")
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+engine = create_engine(DATABASE_URL)
+
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+
+
+
+
+
+
+
